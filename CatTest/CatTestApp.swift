a9a7c24f218f0439d4f76a9e4854353df0bad532
Sergei Volkov <volkovsa@vsa.su>
2021-09-09 11:52:47 +0500

    Initial Commit

---
+++ CatTest/CatTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatTestApp.swift
+//  CatTest
+//
+//  Created by Sergei Volkov on 09.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct CatTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
